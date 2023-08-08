fb0f5d4bd5b05b47049051ab12b096d0a7bfcb5b
moises <moises.huaringa>
2023-08-07 21:01:26 -0500

    init project

---
+++ .changeset/yellow-tables-speak.md
@@ -0,0 +1,8 @@
+---
+"eslint-config-turbomoiseshp": patch
+"@moiseshp/turborepo-core": patch
+"@moiseshp/turborepo-tsconfig": patch
+"@moiseshp/turborepo-utils": patch
+---
+
+init
--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -14,10 +14,10 @@ module.exports = {
       resolve: {
         alias: [
           {
-            find: "@acme/core",
+            find: "@moiseshp/core",
             replacement: path.resolve(
               __dirname,
-              "../../../packages/acme-core/"
+              "../../../packages/@moiseshp/turborepo-core/"
             ),
           },
         ],
